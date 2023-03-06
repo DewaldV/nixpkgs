9733906271e143b55345dd47dcab843082a0c280
DewaldV <dewald.viljoen@pm.me>
2023-03-06 15:37:45 +0000

    flake-utils: multi-system packages

--- flake.nix
+++ flake.nix
@@ -1,21 +1,20 @@
 {
   description = "Personal Nix packages";
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ self.overlay.${system} ];
-      };
-    in rec {
-      ${system} = rec {
+  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay.${system} ];
+        };
+      in rec {
         packages = { rvu-u = pkgs.callPackage ./packages/u { }; };
 
-        overlay =
-          (final: prev: { inherit (self.packages.${system}) ipu6-drivers; });
+        overlay = (final: prev: { inherit (self.packages.${system}) rvu-u; });
 
         formatter = pkgs.nixpkgs-fmt;
-      };
-    };
+      });
 }
